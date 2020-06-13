6bbc38cb49fcf75827134db3396919116bc307ef
vaguebrownfox <foxjournal.nsfw@gmail.com>
2020-06-13 13:05:37 +0530

    first branch commit2

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import {Text} from 'react-native';
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
+// create a component
 const HomeScreen = () => {
-    return <Text>Home screen!</Text>;
+    return (
+        <View style={styles.container}>
+            <Text>Home screen</Text>
+        </View>
+    );
 };
 
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+});
+
+//make this component available to the app
 export default HomeScreen;
